c9dfa375d7358be526899d4e8009aa2b0ee5ae75
liuhuihou <2676034256@qq.com>
2025-05-13 10:38:48 +0800

    稍微加了几个包

--- src/view/register/RegisterFrame.java
+++ src/view/register/RegisterFrame.java
@@ -1,8 +1,10 @@
 package view.register;
-
+import view.FrameUtil;
 import view.game.GameFrame;
-
 import javax.swing.*;
+import java.awt.*;
+
+//未完成 未在主程序引入
 
 public class RegisterFrame extends JFrame {
 
@@ -11,6 +13,52 @@ public class RegisterFrame extends JFrame {
         private JButton submitBtn;
         private JButton resetBtn;
         private GameFrame gameFrame;
+
+        public RegisterFrame(int width, int height) {
+                this.setTitle("Register Frame");
+                this.setLayout(null);
+                this.setSize(width, height);
+                JLabel userLabel = FrameUtil.createJLabel(this, new Point(50, 20), 70, 40, "username:");
+                JLabel passLabel = FrameUtil.createJLabel(this, new Point(50, 80), 70, 40, "password:");
+                username = FrameUtil.createJTextField(this, new Point(120, 20), 120, 40);
+                password = FrameUtil.createJTextField(this, new Point(120, 80), 120, 40);
+
+                submitBtn = FrameUtil.createButton(this, "Confirm", new Point(40, 140), 100, 40);
+                resetBtn = FrameUtil.createButton(this, "Reset", new Point(160, 140), 100, 40);
+
+                submitBtn.addActionListener(e -> {
+                        System.out.println("Username = " + username.getText());
+                        System.out.println("Password = " + password.getText());
+                        if (this.gameFrame != null) {
+                                this.gameFrame.setVisible(true);
+                                this.setVisible(false);
+                        }
+                        //todo: check Register info
+
+                });
+                resetBtn.addActionListener(e -> {
+                        username.setText("");
+                        password.setText("");
+                });
+
+
+
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
+        }
+
+
+        public void setGameFrame(GameFrame gameFrame) {
+                this.gameFrame = gameFrame;
+        }
+
+
+
+
+
 
 
 
