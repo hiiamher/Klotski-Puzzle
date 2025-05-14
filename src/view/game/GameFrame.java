d5b963e850e9123d6696b47e9c1ed4f782dc76b7
Kiki476452 <haiyuezhao06@gmail.com>
2025-05-14 14:53:34 +0800

    在游戏界面中创建了控制方向按钮的版面

--- src/view/game/GameFrame.java
+++ src/view/game/GameFrame.java
@@ -1,6 +1,7 @@
 package view.game;
 
 import controller.GameController;
+import model.Direction;
 import model.MapModel;
 import view.FrameUtil;
 
@@ -15,6 +16,38 @@ public class GameFrame extends JFrame {
 
     private JLabel stepLabel;
     private GamePanel gamePanel;
+    private JButton leftButton,rightButton,upButton,downButton;
+    private JPanel buttonPanel;
+
+    //创建JPanel对象，用于放置方向按钮
+    private JPanel createbuttonPanel() {
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(2, 3));
+        buttonPanel.setBounds(280, 300, 150, 100);
+        upButton = new JButton("↑");
+        upButton.addActionListener(e -> {
+            gamePanel.doMoveUp();
+        });
+        buttonPanel.add(new JButton());
+        buttonPanel.add(upButton);
+        buttonPanel.add(new JButton());
+        leftButton = new JButton("←");
+        leftButton.addActionListener(e -> {
+            gamePanel.doMoveLeft();
+        });
+        buttonPanel.add(leftButton);
+        downButton = new JButton("↓");
+        downButton.addActionListener(e -> {
+            gamePanel.doMoveDown();
+        });
+        buttonPanel.add(downButton);
+        rightButton = new JButton("→");
+        rightButton.addActionListener(e -> {
+            gamePanel.doMoveRight();
+        });
+        buttonPanel.add(rightButton);
+        return buttonPanel;
+    }
 
     public GameFrame(int width, int height, MapModel mapModel) {
         this.setTitle("2025 CS109 Project Demo");
@@ -29,7 +62,7 @@ public class GameFrame extends JFrame {
         this.loadBtn = FrameUtil.createButton(this, "Load", new Point(gamePanel.getWidth() + 80, 210), 80, 50);
         this.stepLabel = FrameUtil.createJLabel(this, "Start", new Font("serif", Font.ITALIC, 22), new Point(gamePanel.getWidth() + 80, 70), 180, 50);
         gamePanel.setStepLabel(stepLabel);
-
+        this.add(createbuttonPanel());
         this.restartBtn.addActionListener(e -> {
             controller.restartGame();
             gamePanel.requestFocusInWindow();//enable key listener
@@ -41,7 +74,10 @@ public class GameFrame extends JFrame {
         });
         //todo: add other button here
         this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);}
+
+
+
     }
 
-}
+
