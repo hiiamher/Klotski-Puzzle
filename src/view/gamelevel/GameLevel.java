d030a22985f346eb586d2298d06d192ee01ca2e6
liuhuihou <2676034256@qq.com>
2025-05-27 11:05:48 +0800

    关卡bug修复，增设关卡

--- src/view/gamelevel/GameLevel.java
+++ src/view/gamelevel/GameLevel.java
@@ -22,6 +22,9 @@ public class GameLevel extends JFrame{
     private JButton level1;
     private JButton level2;
     private JButton level3;
+    private JButton level4;
+    private JButton level5;
+    private JButton level6;
     private JFrame gameFrame;
     private GamePanel gamePanel;
     private MapModel model;
@@ -35,9 +38,13 @@ public class GameLevel extends JFrame{
         this.setLayout(null);
         this.setSize(width, height);
 
-        level1 = FrameUtil.createButton(this, "1", new Point(width*3/4, 80), 100, 40);
-        level2 = FrameUtil.createButton(this, "2", new Point(width*3/4, 160), 100, 40);
-        level3 = FrameUtil.createButton(this, "3", new Point(width*3/4, 240), 100, 40);
+        level1 = FrameUtil.createButton(this, "立马横刀", new Point(width/4, 80), 100, 40);
+        level2 = FrameUtil.createButton(this, "指挥若定", new Point(width/4, 160), 100, 40);
+        level3 = FrameUtil.createButton(this, "将拥曹营", new Point(width/4, 240), 100, 40);
+        level4 = FrameUtil.createButton(this, "左右步兵", new Point(width*2/4, 80), 100, 40);
+        level5 = FrameUtil.createButton(this, "雨声淅沥", new Point(width*2/4, 160), 100, 40);
+        level6 = FrameUtil.createButton(this, "扬帆起航", new Point(width*2/4, 240), 100, 40);
+
 
         level1.addActionListener(e -> {
 
@@ -48,11 +55,19 @@ public class GameLevel extends JFrame{
                     {3, 1, 1, 3},
                     {1, 0, 0, 1}
             };
+            int[][] map2 = new int[][]{
+                    {3, 4, 4, 3},
+                    {3, 4, 4, 3},
+                    {3, 2, 2, 3},
+                    {3, 1, 1, 3},
+                    {1, 0, 0, 1}
+            };
+
 
             this.model.setMatrix(map1);
-            this.model.setOriginalMatrix(map1);
+            this.model.setOriginalMatrix(map2);
             this.gamePanel.clearAllBoxFromPanel();
-            this.gamePanel.initialGame(map1);
+            this.gamePanel.initialGame(map2);
 
 
 
@@ -63,17 +78,24 @@ public class GameLevel extends JFrame{
         level2.addActionListener(e -> {
 
             int[][] map1 = new int[][]{
-                    {0, 1, 1, 3},
-                    {0, 4, 4, 3},
-                    {0, 4, 4, 1},
-                    {0, 1, 1, 1},
-                    {1, 0, 0, 1}
+                    {3, 4, 4, 3},
+                    {3, 4, 4, 3},
+                    {1, 2, 2, 1},
+                    {3, 1, 1, 3},
+                    {3, 0, 0, 3}
+            };
+            int[][] map2 = new int[][]{
+                    {3, 4, 4, 3},
+                    {3, 4, 4, 3},
+                    {1, 2, 2, 1},
+                    {3, 1, 1, 3},
+                    {3, 0, 0, 3}
             };
 
             this.model.setMatrix(map1);
-            this.model.setOriginalMatrix(map1);
+            this.model.setOriginalMatrix(map2);
             this.gamePanel.clearAllBoxFromPanel();
-            this.gamePanel.initialGame(map1);
+            this.gamePanel.initialGame(map2);
 
 
         });
@@ -81,17 +103,97 @@ public class GameLevel extends JFrame{
         level3.addActionListener(e -> {
 
             int[][] map1 = new int[][]{
+                    {0, 4, 4, 0},
                     {3, 4, 4, 3},
+                    {3, 3, 3, 3},
+                    {1, 3, 3, 1},
+                    {2, 2, 1, 1}
+            };
+            int[][] map2 = new int[][]{
+                    {0, 4, 4, 0},
                     {3, 4, 4, 3},
-                    {3, 1, 1, 1},
-                    {3, 1, 1, 1},
-                    {1, 0, 0, 1}
+                    {3, 3, 3, 3},
+                    {1, 3, 3, 1},
+                    {2, 2, 1, 1}
             };
 
             this.model.setMatrix(map1);
-            this.model.setOriginalMatrix(map1);
+            this.model.setOriginalMatrix(map2);
             this.gamePanel.clearAllBoxFromPanel();
-            this.gamePanel.initialGame(map1);
+            this.gamePanel.initialGame(map2);
+
+
+        });
+
+        level4.addActionListener(e -> {
+            int[][] map1 = new int[][]{
+                    {1, 4, 4, 1},
+                    {1, 4, 4, 1},
+                    {3, 3, 3, 3},
+                    {3, 3, 3, 3},
+                    {0, 2, 2, 0}
+            };
+            int[][] map2 = new int[][]{
+                    {1, 4, 4, 1},
+                    {1, 4, 4, 1},
+                    {3, 3, 3, 3},
+                    {3, 3, 3, 3},
+                    {0, 2, 2, 0}
+            };
+
+            this.model.setMatrix(map1);
+            this.model.setOriginalMatrix(map2);
+            this.gamePanel.clearAllBoxFromPanel();
+            this.gamePanel.initialGame(map2);
+
+
+        });
+
+        level5.addActionListener(e -> {
+            int[][] map1 = new int[][]{
+                    {3, 4, 4, 1},
+                    {3, 4, 4, 1},
+                    {3, 2, 2, 3},
+                    {3, 3, 0, 3},
+                    {1, 3, 0, 1}
+            };
+            int[][] map2 = new int[][]{
+                    {3, 4, 4, 1},
+                    {3, 4, 4, 1},
+                    {3, 2, 2, 3},
+                    {3, 3, 0, 3},
+                    {1, 3, 0, 1}
+            };
+
+            this.model.setMatrix(map1);
+            this.model.setOriginalMatrix(map2);
+            this.gamePanel.clearAllBoxFromPanel();
+            this.gamePanel.initialGame(map2);
+
+
+        });
+
+        level6.addActionListener(e -> {
+            int[][] map1 = new int[][]{
+                    {1, 4, 4, 1},
+                    {1, 4, 4, 1},
+                    {1, 0, 0, 1},
+                    {3, 0, 0, 3},
+                    {3, 0, 0, 3}
+            };
+            int[][] map2 = new int[][]{
+                    {1, 4, 4, 1},
+                    {1, 4, 4, 1},
+                    {1, 0, 0, 1},
+                    {3, 0, 0, 3},
+                    {3, 0, 0, 3}
+            };
+
+            this.model.setMatrix(map1);
+            this.model.setOriginalMatrix(map2);
+            this.gamePanel.clearAllBoxFromPanel();
+            this.gamePanel.initialGame(map2);
+
 
 
         });
